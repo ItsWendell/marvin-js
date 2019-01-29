5acebc6308c2d804b6b0ce93ad532b09744869fc
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2019-01-29 09:14:59 +0100

    chore: export keepawake as module in index

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,5 +2,6 @@ import * as factoids from './factoids';
 import * as logger from './logger';
 import * as commands from './commands';
 import * as intra42 from './intra42';
+import * as keepawake from './keepawake';
 
-export { factoids, logger, commands, intra42 };
+export { factoids, logger, commands, intra42, keepawake };
