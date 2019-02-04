e0972fc64d329ce0fa3dce535a55670da0ef066e
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2019-02-04 19:50:29 +0100

    refactor: set timeouts for factoids to 5 seconds

--- src/providers/vm/vm.js
+++ src/providers/vm/vm.js
@@ -4,15 +4,16 @@ import axios from 'axios';
 
 export function runScriptContext(code, message = {}) {
     const appContext = new AppContext(message);
-
+    const timeout = 5000; // Timeout in milliseconds
     const vm = new VM({
         sandbox: {
             ...appContext,
             axios: axios.create({
-                // Auto timeout after 30 seconds.
-                timeout: 30000
+                // Auto timeout after 5 seconds.
+                timeout: timeout
             }),
-        }
+        },
+        timeout: timeout
     });
 
     try {
