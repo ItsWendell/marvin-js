9708849c0bc74ebc2b636e65c6154c79294d4710
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2019-06-25 13:20:24 +0200

    chore: disable jobs for intra temporary

--- src/modules/intra42/jobs.js
+++ src/modules/intra42/jobs.js
@@ -1,15 +1,14 @@
-import scheduler from '../../providers/scheduler';
-import { sendCoalitionStats, fetchCoalitionStats, coalitionInterval } from './intra42';
+// import scheduler from '../../providers/scheduler';
+// import { sendCoalitionStats, fetchCoalitionStats, coalitionInterval } from './intra42';
 
 export function register() {
   // Send daily updates to general channel with coalition updates
-  scheduler.scheduleJob('0 10 * * *', () => {
-    console.log('[Jobs] Sending Coalition Stats...');
-    sendCoalitionStats();
-  });
-
-  scheduler.scheduleJob(`*/${coalitionInterval} * * * *`, () => {
-    console.log('[Jobs] Fetching coalition updates...');
-    fetchCoalitionStats();
-  });
+  // scheduler.scheduleJob('0 10 * * *', () => {
+  //   console.log('[Jobs] Sending Coalition Stats...');
+  //   sendCoalitionStats();
+  // });
+  // scheduler.scheduleJob(`*/${coalitionInterval} * * * *`, () => {
+  //   console.log('[Jobs] Fetching coalition updates...');
+  //   fetchCoalitionStats();
+  // });
 }
